131df9646be5e77ec48c9e55e49fde334aeb53a8
nico <brun.nicolas21@gmail.com>
2016-11-27 15:32:01 +0100

    fix missing import + assignemnt operator

--- jwt.go
+++ jwt.go
@@ -14,6 +14,7 @@ import (
 	"errors"
 	"fmt"
 	"hash"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -108,7 +109,7 @@ func (s hmacSigner) Sign(p interface{}) (string, error) {
 	if s.TTL > 0 {
 		payload.Expire = time.Now().Add(time.Second * time.Duration(s.TTL))
 	}
-	buf, err := json.Marshal(payload)
+	buf, err = json.Marshal(payload)
 	if err != nil {
 		return "", err
 	}
